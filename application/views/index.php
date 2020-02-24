45dc2fa69e2a6d76e1c15d07dbe186c6b42f8bc5
moontai0724 <moontai0724@gmail.com>
2020-02-24 11:33:17 +0800

    更新提示

--- application/views/index.php
+++ application/views/index.php
@@ -7,7 +7,7 @@
 	<ul class="ui list large">
 		<li>透過 git 資料夾找到原始碼放的地方 (keyword: git remote address file)</li>
 		<li>查看原始碼，試圖找出漏洞</li>
-		<li>找到登入頁面，透過 sql injection 登入，以及找到密碼</li>
+		<li>找到登入頁面，透過 sql injection 登入，以及找到管理員密碼。</li>
 	</ul>
 </div>
 
