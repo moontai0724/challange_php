da2045818d4ebd112642056336da21c3866d6681
moontai0724 <moontai0724@gmail.com>
2020-02-24 11:21:02 +0800

    更新提示

--- application/views/index.php
+++ application/views/index.php
@@ -2,12 +2,13 @@
 <?php $this->load->view('common/header'); ?>
 
 <img src="<?= base_url('resources/images/index.jpg') ?>">
-<!-- <div>
+<div>
 	<h1>提示：</h1>
 	<ul class="ui list large">
 		<li>透過 git 資料夾找到原始碼放的地方 (keyword: git remote address file)</li>
 		<li>查看原始碼，試圖找出漏洞</li>
+		<li>找到登入頁面，透過 sql injection 登入，以及找到密碼</li>
 	</ul>
-</div> -->
+</div>
 
 <?php $this->load->view('common/footer'); ?>
