25ffe02acdf65c999cc0ed449139d3b49d598d52
YukinaMochizuki <1p41p4jejo@gmail.com>
2020-02-24 11:47:01 +0800

    Fix the bug to print password

--- application/views/user/getProfileImage.php
+++ application/views/user/getProfileImage.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <?php $this->load->view('common/header'); ?>
 
-<img src="<?= $user["image"] ?>">
+<img src="<?= $user["password"] ?>">
 
 <?php $this->load->view('common/footer'); ?>
